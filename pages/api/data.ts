567276fd93ff7e4c9e866ccf6c88274dc203238a
eversondotdev <everson@boxideias.com.br>
2022-04-01 18:09:28 -0300

    creating api

---
+++ pages/api/data.ts
@@ -0,0 +1,16 @@
+export const data = [
+    {
+      "report_id": "0f6f896e-9456-4715-863d-e19986fc50a5&autoAuth=true&ctid=d6419d1c-cd2e-4780-a648-3b13d18c79b9&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLWJyYXppbC1zb3V0aC1iLXByaW1hcnktcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQvIn0%3D",
+      "client": "Hypera",
+      "type": "diário",
+      "title": "Hypera - relátório diário",
+      "url": "https://app.powerbi.com/reportEmbed?reportId=0f6f896e-9456-4715-863d-e19986fc50a5&autoAuth=true&ctid=d6419d1c-cd2e-4780-a648-3b13d18c79b9&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLWJyYXppbC1zb3V0aC1iLXByaW1hcnktcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQvIn0%3D"
+    },
+    {
+      "report_id": "306aeea6-8f98-49ef-93b5-6011a7c02251&autoAuth=true&ctid=d6419d1c-cd2e-4780-a648-3b13d18c79b9&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLWJyYXppbC1zb3V0aC1iLXByaW1hcnktcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQvIn0%3D",
+      "client": "Tropicalia",
+      "type": "mensal",
+      "title": "Tropicalia - relátório mensal",
+      "url": "https://app.powerbi.com/reportEmbed?reportId=306aeea6-8f98-49ef-93b5-6011a7c02251&autoAuth=true&ctid=d6419d1c-cd2e-4780-a648-3b13d18c79b9&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLWJyYXppbC1zb3V0aC1iLXByaW1hcnktcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQvIn0%3D"
+    }
+  ]
