c2d3d9570066453db401f6d7924ad408e2ef89a2
eversondotdev <everson@boxideias.com.br>
2022-04-08 14:42:18 -0300

    test interface api

--- pages/reports/index.tsx
+++ pages/reports/index.tsx
@@ -2,6 +2,11 @@ import useSWR from 'swr'
 
 const fetcher = (url:any) => fetch(url).then((res) => res.json())
 
+export interface ReportProps {
+  id: number,
+  name: string
+}
+
 export default function Index() {
   const { data, error } = useSWR('/api/reports', fetcher)
 
@@ -10,8 +15,8 @@ export default function Index() {
 
   return (
     <ul>
-      {data.map((p, i) => (
-        <div key={i}>
+      {data.map((p:ReportProps) => (
+        <div key={p.id}>
             {p.name}
         </div>
       ))}
