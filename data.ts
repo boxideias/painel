2798db65738c80c1c868673a8d8ea973aac6bd41
eversondotdev <everson@boxideias.com.br>
2022-04-20 14:35:04 -0300

    new links public dash and name changed

--- data.ts
+++ data.ts
@@ -1,11 +1,11 @@
 export const reports = [
   {
-    id: "hypera",
-    name: "Hypera",
-    dashboard_url: "https://app.powerbi.com/reportEmbed?reportId=b6abef8e-7409-4903-8416-c011b03c2a66&autoAuth=true&ctid=d6419d1c-cd2e-4780-a648-3b13d18c79b9&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLWJyYXppbC1zb3V0aC1iLXByaW1hcnktcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQvIn0%3D",
+    id: "h",
+    name: "h",
+    dashboard_url: "https://app.powerbi.com/view?r=eyJrIjoiZGM0ODhhNGEtYTVjMC00NDI5LTkwNGItOWZkYTMwNDM1ZDBmIiwidCI6ImQ2NDE5ZDFjLWNkMmUtNDc4MC1hNjQ4LTNiMTNkMThjNzliOSJ9",
     reports: [
       {
-        report_url: "hypera_semanal_4465244658.pdf",
+        report_url: "h.pdf",
         date_started: "01/04/2022",
         date_ended: "07/04/2022",
         type: "Semanal",
@@ -13,7 +13,7 @@ export const reports = [
         year: 2022,
       },
       {
-        report_url: "hypera_mensal_4462144651.pdf",
+        report_url: "h.pdf",
         date_started: "01/03/2022",
         date_ended: "31/03/2022",
         type: "Mensal",
@@ -23,12 +23,12 @@ export const reports = [
     ]
   },
   {
-    id: "revest",
-    name: "Revest",
-    dashboard_url: "https://app.powerbi.com/reportEmbed?reportId=4d193e66-6965-4a3e-b196-feda12b104ee&autoAuth=true&ctid=d6419d1c-cd2e-4780-a648-3b13d18c79b9&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLWJyYXppbC1zb3V0aC1iLXByaW1hcnktcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQvIn0%3D",
+    id: "r",
+    name: "r",
+    dashboard_url: "https://app.powerbi.com/view?r=eyJrIjoiMGZhMzcxYmUtODM1YS00ZDY1LTgzOGQtMmM4MGYyOTQ5ZTcxIiwidCI6ImQ2NDE5ZDFjLWNkMmUtNDc4MC1hNjQ4LTNiMTNkMThjNzliOSJ9",
     reports:[
       {
-        report_url: "revest_mensal_4462144651.pdf",
+        report_url: "r.pdf",
         date_started: "01/03/2022",
         date_ended: "31/03/2022",
         type: "Mensal",
