27aa5bbe412be953cc501ec640cf40f3a27c15b1
eversondotdev <everson@boxideias.com.br>
2022-08-29 16:43:43 -0300

    hypera v2

--- data.ts
+++ data.ts
@@ -23,6 +23,29 @@ export const reports = [
     ]
   },
   {
+    id: "h2",
+    name: "h",
+    dashboard_url: "https://app.powerbi.com/view?r=eyJrIjoiMzg3NWVmM2UtN2VlZC00ZGJjLWFlN2YtNjMyZWExYTFkNWFmIiwidCI6ImQ2NDE5ZDFjLWNkMmUtNDc4MC1hNjQ4LTNiMTNkMThjNzliOSJ9",
+    reports: [
+      {
+        report_url: "h.pdf",
+        date_started: "01/04/2022",
+        date_ended: "07/04/2022",
+        type: "Semanal",
+        month: "Abril",
+        year: 2022,
+      },
+      {
+        report_url: "h.pdf",
+        date_started: "01/03/2022",
+        date_ended: "31/03/2022",
+        type: "Mensal",
+        month: "Março",
+        year: 2022,
+      }
+    ]
+  },
+  {
     id: "r",
     name: "r",
     dashboard_url: "https://app.powerbi.com/view?r=eyJrIjoiMGZhMzcxYmUtODM1YS00ZDY1LTgzOGQtMmM4MGYyOTQ5ZTcxIiwidCI6ImQ2NDE5ZDFjLWNkMmUtNDc4MC1hNjQ4LTNiMTNkMThjNzliOSJ9",
